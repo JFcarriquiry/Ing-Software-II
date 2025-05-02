abe996ca43ba7a717d84580ef512f8b3cde501bb
Nahuecur <ncurbelo@correo.um.edu.uy>
2025-05-01 21:07:16 -0300

    cargue la perdiz

--- backend/seed.sql
+++ backend/seed.sql
@@ -2,4 +2,5 @@
 INSERT INTO restaurants (name, description, phone, email, address, latitude, longitude, seats_total)
 VALUES
 ('La Pasiva', 'Chivitos y panchos clásicos', '24001234', 'info@lapasiva.com.uy', 'Av. 18 de Julio 1234, Montevideo', -34.90328, -56.18816, 120),
-('Café Brasilero', 'Café histórico con ambiente bohemio', '29150123', 'contacto@cafebrasilero.com.uy', 'Ituzaingó 1447, Montevideo', -34.90757, -56.20312, 40);
+('Café Brasilero', 'Café histórico con ambiente bohemio', '29150123', 'contacto@cafebrasilero.com.uy', 'Ituzaingó 1447, Montevideo', -34.90757, -56.20312, 40),
+('La Perdiz', 'Parrillada', '25081404', 'curbel@gmail.com','Guipúzcoa 350, Montevideo', -34.924832961255355, -56.15683872031505, 120);
