c6d43722448e1cf267ad6e58b3a28be6190ffa0c
JFcarriquiry <juancarr502009@gmail.com>
2025-06-10 19:13:11 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,6 +34,7 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 });
